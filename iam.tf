848ad1c24c5f3b0ca222f440f11dbfc4df1d1b01
Rafael Paiva <rafa_klose@hotmail.com>
2024-09-22 23:09:06 -0300

    fix:Terraform fmt

--- iam.tf
+++ iam.tf
@@ -65,25 +65,25 @@ resource "aws_iam_role" "ecr-role" {
   name = "ecr-role"
 
   assume_role_policy = jsonencode({
-    Statement: [
-        {
-            Effect = "Allow",
-            Action = "sts:AssumeRoleWithWebIdentity",
-            Principal = {
-                "Federated": "arn:aws:iam::416098782400:oidc-provider/token.actions.githubusercontent.com"
-            },
-            Condition = {
-                StringEquals = {
-                    "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
-                    "token.actions.githubusercontent.com:sub" = "repo:rafapaivadeandrade/devops-docker-containers:ref:refs/heads/main"
-                    
-                }
-            }
+    Statement : [
+      {
+        Effect = "Allow",
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Principal = {
+          "Federated" : "arn:aws:iam::416098782400:oidc-provider/token.actions.githubusercontent.com"
+        },
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
+            "token.actions.githubusercontent.com:sub" = "repo:rafapaivadeandrade/devops-docker-containers:ref:refs/heads/main"
+
+          }
         }
+      }
     ]
     Version = "2012-10-17"
   })
-    inline_policy {
+  inline_policy {
     name = "ecr-app-permissions"
     policy = jsonencode({
       Statement = [{
