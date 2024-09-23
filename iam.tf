157c15e3d055d6c14ade71780c23a5885b433b5c
Rafael Paiva <rafa_klose@hotmail.com>
2024-09-23 00:18:39 -0300

    fix:fix fmt

--- iam.tf
+++ iam.tf
@@ -48,6 +48,12 @@ resource "aws_iam_role" "tf-role" {
           Action   = "iam:*"
           Effect   = "Allow"
           Resource = "*"
+        },
+        {
+          Sid      = "Statement3"
+          Action   = "s3:*"
+          Effect   = "Allow"
+          Resource = "*"
         }
       ]
     })
--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 terraform {
   required_providers {
     aws = {
@@ -6,8 +5,28 @@ terraform {
       version = "5.49.0"
     }
   }
+  backend "s3" {
+    bucket = "rocketseat-ia-staging-9f47d8ca"
+    key    = "state/terraform.tfstate"
+    region = "us-east-2"
+  }
 }
-
 provider "aws" {
-  region = "us-east-2" 
+  region = "us-east-2"
+}
+resource "aws_s3_bucket" "terraform-state" {
+  bucket        = "rocketseat-ia-staging-9f47d8ca"
+  force_destroy = true
+  lifecycle {
+    prevent_destroy = true
+  }
+  tags = {
+    IAC = "True"
+  }
+}
+resource "aws_s3_bucket_versioning" "terraform-state" {
+  bucket = "rocketseat-iac-staging-9f47d8ca"
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
